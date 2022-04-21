808077da57589d5d19647dbdd799a82544fba576
FabienFr <fabien@xaeos.com>
2022-04-21 23:54:32 +0200

    modif test on deploy

--- src/components/Form2.vue
+++ src/components/Form2.vue
@@ -3,7 +3,12 @@
         <form>
             <section class="w-full my-10 pt-5 px-5 bg-gray-50 rounded-xl">
                 <h1 class="font-supercell text-lg text-gray-700 py5">Compte</h1>
-                <select v-model="formData.hdv" class="mx-auto my-5 w-full border border-gray-300 dark:border-gray-700 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-indigo-600 text-gray-800 bg-transparent dark:text-gray-100">
+                <select
+                    @input="toggleInput"
+                    v-model="formData.hdv"
+                    class="mx-auto my-5 w-full border border-gray-300 dark:border-gray-700 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-indigo-600 text-gray-800 bg-transparent dark:text-gray-100"
+                    required
+                >
                     <option>Hdv 14</option>
                     <option>Hdv 13</option>
                     <option>Hdv 12</option>
@@ -13,11 +18,12 @@
                 </select>
                 <!-- Pseudo -->
                 <input
+                    @input="toggleInput"
                     v-model="formData.username"
                     type="text"
                     id="username"
                     name="username"
-                    class="mx-auto w-full mb-7  border border-gray-300 dark:border-gray-700 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-indigo-600 text-gray-800 bg-transparent dark:text-gray-100"
+                    class="mx-auto w-full mb-7 border border-gray-300 dark:border-gray-700 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-indigo-600 text-gray-800 bg-transparent dark:text-gray-100"
                     placeholder="Pseudo"
                     required
                 />
@@ -27,148 +33,133 @@
                 <h1 class="font-supercell text-lg text-gray-700 mb-5 py5">
                     Niveau de league souhaité
                 </h1>
-                <div
-                    class="w-full grid sm:grid-cols-1 md:grid-cols-2 gap-8"
-                >
-                    <a
+                <div class="w-full grid sm:grid-cols-1 md:grid-cols-2 gap-8">
+                    <button
+                        value="cactus"
+                        @click.prevent="addChoice"
                         aria-label="card 1"
-                        href="javascript:void(0)"
                         class="bg-white dark:bg-gray-800 rounded focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:outline-none focus:bg-gray-100 hover:bg-gray-100"
                     >
-                        <div class="shadow px-8 py-6 flex items-center">
-                            <div class="p-4 bg-indigo-700 rounded">
-                                <img
-                                    class="h-8 w-8"
-                                    src="../assets/noun-cactus.svg"
-                                    alt="icon"
-                                />
-                            </div>
-
-                            <div class="ml-6">
+                        <div class="shadow px-8 py-6 flex items-center justify-center">
+                            <div>
                                 <h3
-                                    class="mb-1 leading-5 text-gray-800 dark:text-gray-100 font-bold text-2xl"
+                                    class="mb-1 leading-5 text-gray-800 dark:text-gray-100 font-supercell font-bold text-2xl"
                                 >
-                                    CACTUS
+                                    Cactus
                                 </h3>
-                                <p
-                                    class="text-gray-600 dark:text-gray-400 text-sm tracking-normal font-normal leading-5"
-                                >
-                                    MASTER 1 
-                                </p>
                                 <p
                                     class="text-gray-600 dark:text-gray-400 text-sm tracking-normal font-normal leading-5"
                                 >
                                     full hdv14
                                 </p>
+                                <p
+                                    class="text-gray-600 dark:text-gray-400 text-sm tracking-normal font-supercell font-normal leading-5"
+                                >
+                                    Master 1
+                                </p>
                             </div>
                         </div>
-                    </a>
-                    <a
+                    </button>
+                    <button
+                        value="kipik"
+                        @click.prevent="addChoice"
                         aria-label="card 2"
-                        href="javascript:void(0)"
                         class="bg-white dark:bg-gray-800 rounded focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:outline-none focus:bg-gray-100 hover:bg-gray-100"
                     >
-                        <div class="shadow px-8 py-6 flex items-center">
-                            <div class="p-4 bg-indigo-700 rounded text-white">
-                                <img
-                                    class="h-8 w-8"
-                                    src="../assets/noun-prick.svg"
-                                    alt="icon"
-                                />
-                            </div>
-                            <div class="ml-6">
+                        <div class="shadow px-8 py-6 flex items-center justify-center">
+                            <div>
                                 <h3
-                                    class="mb-1 leading-5 text-gray-800 dark:text-gray-100 font-bold text-2xl"
+                                    class="mb-1 leading-5 text-gray-800 dark:text-gray-100 font-supercell font-bold text-2xl"
                                 >
-                                    KIPIK
+                                    Kipik
                                 </h3>
                                 <p
                                     class="text-gray-600 dark:text-gray-400 text-sm tracking-normal font-normal leading-5"
                                 >
-                                    MASTER 2 
+                                    full hdv 14
                                 </p>
                                 <p
-                                    class="text-gray-600 dark:text-gray-400 text-sm tracking-normal font-normal leading-5"
+                                    class="text-gray-600 dark:text-gray-400 text-sm tracking-normal font-supercell font-normal leading-5"
                                 >
-                                    full hdv 14 
+                                    Master 2
                                 </p>
                             </div>
                         </div>
-                    </a>
-                    <a
+                    </button>
+                    <button
+                        value="rose"
+                        @click.prevent="addChoice"
                         aria-label="card 3"
-                        href="javascript:void(0)"
                         class="bg-white dark:bg-gray-800 rounded focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:outline-none focus:bg-gray-100 hover:bg-gray-100"
                     >
-                        <div class="shadow px-8 py-6 flex items-center">
-                            <div class="p-4 bg-indigo-700 rounded text-white">
-                                <img
-                                    class="h-8 w-8"
-                                    src="../assets/noun-rose.svg"
-                                    alt="icon"
-                                />
-                            </div>
-                            <div class="ml-6">
+                        <div class="shadow px-8 py-6 flex items-center justify-center">
+                            <div>
                                 <h3
-                                    class="mb-1 leading-5 text-gray-800 dark:text-gray-100 font-bold text-2xl"
+                                    class="mb-1 leading-5 text-gray-800 dark:text-gray-100 font-supercell font-bold text-2xl"
                                 >
-                                    ROSE
+                                    Rose
                                 </h3>
                                 <p
                                     class="text-gray-600 dark:text-gray-400 text-sm tracking-normal font-normal leading-5"
                                 >
-                                    MASTER 3 
+                                    mixte hdv 13-14
                                 </p>
                                 <p
-                                    class="text-gray-600 dark:text-gray-400 text-sm tracking-normal font-normal leading-5"
+                                    class="text-gray-600 dark:text-gray-400 text-sm tracking-normal font-supercell font-normal leading-5"
                                 >
-                                  mixte hdv 13-14 
+                                    Master 3
                                 </p>
                             </div>
                         </div>
-                    </a>
-                    <a
+                    </button>
+                    <button
+                        @click.prevent="addChoice"
+                        value="paquerette"
                         aria-label="card 4"
-                        href="javascript:void(0)"
                         class="bg-white dark:bg-gray-800 rounded focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:outline-none focus:bg-gray-100 hover:bg-gray-100"
                     >
-                        <div class="shadow px-8 py-6 flex items-center">
-                            <div class="p-4 bg-indigo-700 rounded text-white">
-                                <img
-                                    class="h-8 w-8"
-                                    src="../assets/noun-daisy.svg"
-                                    alt="icon"
-                                />
-                            </div>
+                        <div class="shadow px-8 py-6 flex items-center justify-center">
                             <div class="ml-6">
                                 <h3
-                                    class="mb-1 leading-5 text-gray-800 dark:text-gray-100 font-bold text-2xl"
+                                    class="mb-1 leading-5 text-gray-800 dark:text-gray-100 font-supercell font-bold text-2xl"
                                 >
-                                    PAQUERETTE
+                                    Pâquerette
                                 </h3>
-                                <p
-                                    class="text-gray-600 dark:text-gray-400 text-sm tracking-normal font-normal leading-5"
-                                >
-                                    KRISTAL 3
-                                </p>
                                 <p
                                     class="text-gray-600 dark:text-gray-400 text-sm tracking-normal font-normal leading-5"
                                 >
                                     mixte hdv 9-14
                                 </p>
+                                <p
+                                    class="text-gray-600 dark:text-gray-400 text-sm tracking-normal font-supercell font-normal leading-5"
+                                >
+                                    Kristal 3
+                                </p>
                             </div>
                         </div>
-                    </a>
+                    </button>
                 </div>
             </section>
 
             <div class="w-full my-10 py-5 px-5 bg-gray-50 rounded-xl">
-              <button
-                class=" w-full bg-indigo-600 transition duration-150 ease-in-out hover:bg-indigo-600 rounded text-supercell text-white px-8 py-2 text-sm focus:outline-none"
-                type="submit"
-              >
-                Envoyer
-              </button>
+                <button
+                    v-if="!infoSubmit"
+                    @click.prevent="formSubmit"
+                    class="w-full bg-indigo-600 transition duration-150 ease-in-out hover:bg-indigo-600 rounded text-supercell text-white px-8 py-4 text-sm focus:outline-none"
+                    type="submit"
+                >
+                    Envoyer
+                </button>
+            </div>
+
+            <div
+                v-if="infoSubmit"
+                class="w-full my-10 py-5 px-5 bg-green-100 rounded-xl"
+            >
+                <h2>Récap</h2>
+                <p>Hdv : {{ formData.hdv }}</p>
+                <p>Username : {{ formData.username }}</p>
+                <p>League choisie: {{ formData.league }}</p>
             </div>
 
             <!-- <section class="w-full py-5 bg-gray-50 rounded">
@@ -191,14 +182,26 @@ export default {
     name: 'form',
     data() {
         return {
-          formData: {
-            hdv: '',
-            username: '',
-            newUsername: '',
-            league: ''
-          }
+            formData: {
+                hdv: '',
+                username: '',
+                newUsername: '',
+                league: '',
+            },
+            infoSubmit: false,
         }
     },
-    methods: {},
+    methods: {
+        addChoice() {
+            this.league.push({ value })
+            console.log(league)
+        },
+        formSubmit() {
+            this.infoSubmit = true
+        },
+        toggleInput() {
+            this.infoSubmit = false
+        },
+    },
 }
 </script>
