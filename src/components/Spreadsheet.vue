4f77d56fe93133764eea9956c6ae7f0765cdeb44
FabienFr <fabien@xaeos.com>
2022-04-23 16:54:40 +0200

    new sections but grid system is out

--- src/components/Spreadsheet.vue
+++ src/components/Spreadsheet.vue
@@ -1,107 +1,100 @@
 <template>
-    <div class="container mx-auto">
-        <form>
-            <section
-                class="w-full mt-10 pt-5 px-5 p-6 hover:shadow-lg hover:bg-lightgrey rounded-xl"
+    <form class="container mx-auto">
+        <section
+            class="min-w-full mt-10 pt-5 px-5 p-6 hover:shadow-lg hover:bg-lightgrey rounded-xl"
+        >
+            <h1 class="font-supercell text-3xl text-gray-700 py5 text-rouge">
+                Compte
+            </h1>
+            <h2
+                class="font-supercell text-2xl text-gray-700 py5 mt-6 text-orange"
             >
-                <h1 class="font-supercell text-3xl text-gray-700 py5 text-rouge">
-                    Compte
-                </h1>
-                <h2
-                    class="font-supercell text-2xl text-gray-700 py5 mt-6 text-orange"
-                >
-                    Pseudo
-                </h2>
-                <div class="container mx-auto">
-                    <input
-                        class="mx-auto my-5 w-full border border-orange pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-rouge text-orange"
-                        v-model="pseudo"
-                    />
-                    <!-- <option v-for="item in liste" :key="item.id">
+                Pseudo
+            </h2>
+            <div class="container mx-auto">
+                <input
+                    class="mx-auto my-5 w-full border border-orange pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-rouge text-orange"
+                    v-model="pseudo"
+                />
+                <!-- <option v-for="item in liste" :key="item.id">
                             {{ item.pseudo }}
                         </option> -->
-                </div>
-                <div>
-                    <div class="items-left justify-center w-full mb-6">
-                        <label
-                            for="toogleA"
-                            class="flex items-center cursor-pointer"
-                        >
-                            <!-- toggle -->
-                            <div class="relative">
-                                <!-- input -->
-                                <input
-                                    id="toogleA"
-                                    type="checkbox"
-                                    class="sr-only"
-                                    v-model="toggleNewPseudo"
-                                />
-                                <!-- line -->
-                                <div
-                                    class="w-10 h-4 bg-lightgrey rounded-full shadow-inner"
-                                ></div>
-                                <!-- dot -->
-                                <div
-                                    class="dot absolute w-6 h-6 bg-beige rounded-full shadow -left-1 -top-1 transition"
-                                ></div>
-                            </div>
-                            <!-- label -->
-                            <div class="ml-3 text-beige text-xl tracking-normal font-bold">
-                                Mon pseudo n'est pas dans la liste
-                            </div>
-                        </label>
-                    </div>
-                    <div v-if="toggleNewPseudo">
-                        <p
-                            class="font-supercell  text-2xl text-gray-700 py5 mt-2 text-orange"
-                        >
-                            Ajouter mon pseudo
-                        </p>
-                        <input
-                            v-model="newPseudo"
-                            class="mx-auto my-5 w-full border border-orange dark:border-gray-700 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-orange text-orange bg-transparent dark:text-gray-100"
-                        />
-                    </div>
-                </div>
-                <div
-                    v-if="pseudo || newPseudo"
-                    class="w-full pt-2"
-                >
-                    <h1
-                        class="font-supercell text-2xl text-gray-700 text-orange"
+            </div>
+            <div>
+                <div class="items-left justify-center w-full mb-6">
+                    <label
+                        for="toogleA"
+                        class="flex items-center cursor-pointer"
                     >
-                        Hdv
-                    </h1>
-                    <div class="container mx-auto">
-                        <div class="container mx-auto">
-                            <select
-                                @input="toggleInput"
-                                v-model="hdv"
-                                class="mx-auto mt-5 w-full border border-orange dark:border-gray-700 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-orange text-orange bg-transparent dark:text-gray-100"
-                                required
-                            >
-                                <option>Hdv 14</option>
-                                <option>Hdv 13</option>
-                                <option>Hdv 12</option>
-                                <option>Hdv 11</option>
-                                <option>Hdv 10</option>
-                                <option>Hdv 09</option>
-                            </select>
+                        <!-- toggle -->
+                        <div class="relative">
+                            <!-- input -->
+                            <input
+                                id="toogleA"
+                                type="checkbox"
+                                class="sr-only"
+                                v-model="toggleNewPseudo"
+                            />
+                            <!-- line -->
+                            <div
+                                class="w-10 h-4 bg-lightgrey rounded-full shadow-inner"
+                            ></div>
+                            <!-- dot -->
+                            <div
+                                class="dot absolute w-6 h-6 bg-beige rounded-full shadow -left-1 -top-1 transition"
+                            ></div>
                         </div>
+                        <!-- label -->
+                        <div
+                            class="ml-3 text-beige text-xl tracking-normal font-bold"
+                        >
+                            Mon pseudo n'est pas dans la liste
+                        </div>
+                    </label>
+                </div>
+                <div v-if="toggleNewPseudo">
+                    <p
+                        class="font-supercell text-2xl text-gray-700 py5 mt-2 text-orange"
+                    >
+                        Ajouter mon pseudo
+                    </p>
+                    <input
+                        v-model="newPseudo"
+                        class="mx-auto my-5 w-full border border-orange dark:border-gray-700 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-orange text-orange bg-transparent dark:text-gray-100"
+                    />
+                </div>
+            </div>
+            <div v-if="pseudo || newPseudo" class="w-full pt-2">
+                <h1 class="font-supercell text-2xl text-gray-700 text-orange">
+                    Hdv
+                </h1>
+                <div class="container mx-auto">
+                    <div class="container mx-auto">
+                        <select
+                            @input="toggleInput"
+                            v-model="hdv"
+                            class="mx-auto mt-5 w-full border border-orange dark:border-gray-700 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-orange text-orange bg-transparent dark:text-gray-100"
+                            required
+                        >
+                            <option>Hdv 14</option>
+                            <option>Hdv 13</option>
+                            <option>Hdv 12</option>
+                            <option>Hdv 11</option>
+                            <option>Hdv 10</option>
+                            <option>Hdv 09</option>
+                        </select>
                     </div>
                 </div>
-            </section>
-        </form>
-    </div>
-
-    <div v-if="hdv" class="container mx-auto">
+            </div>
+        </section>
         <section
-            class="w-full my-10 pt-5 px-5 p-6 hover:shadow-lg hover:bg-lightgrey rounded-xl"
+            v-if="hdv"
+            class="min-w-full my-10 pt-5 px-5 p-6 hover:shadow-lg hover:bg-lightgrey rounded-xl"
         >
             <h1 class="font-supercell text-3xl mb-5 text-rouge">
                 Niveau de ligue souhaité
             </h1>
-            <div class="w-full grid sm:grid-cols-1 md:grid-cols-2 gap-8">
+            <div class="grid sm:grid-cols-1 md:grid-cols-2 gap-8">
                 <button
                     value="cactus"
                     @click.prevent="addChoice"
@@ -229,43 +222,43 @@
                 </button>
             </div>
         </section>
-
-        <div class="w-full my-10 py-5 px-5 bg-gray-50 rounded-xl">
-            <button
-                v-if="!infoSubmit"
-                @click.prevent="formSubmit"
-                class="w-full border border-beige hover:bg-beige transition duration-150 ease-in-out focus:ring-orange rounded-lg font-supercell text-beige hover:text-white px-8 py-4 text-lg focus:outline-none"
-                type="submit"
+        <section v-if="hdv" class="min-w-full">
+            <div class="w-full my-10 py-5 px-5 bg-gray-50 rounded-xl">
+                <button
+                    v-if="!infoSubmit"
+                    @click.prevent="formSubmit"
+                    class="w-full border border-beige hover:bg-beige transition duration-150 ease-in-out focus:ring-orange rounded-lg font-supercell text-beige hover:text-white px-8 py-4 text-lg focus:outline-none"
+                    type="submit"
+                >
+                    Envoyer
+                </button>
+            </div>
+            <div
+                v-if="infoSubmit"
+                class="container mx-auto w-full my-10 py-5 px-5 bg-gradient-to-bl opacity-60 from-beige to-orange rounded-xl"
             >
-                Envoyer
-            </button>
-        </div>
-
-        <div
-            v-if="infoSubmit"
-            class="container mx-auto w-full my-10 py-5 px-5 bg-gradient-to-bl opacity-60 from-beige to-orange rounded-xl"
-        >
-            <h2 class="font-supercell text-white text-3xl mb-10">Récap</h2>
-            <p class="font-supercell text-white text-2xl">
-                Pseudo : {{ pseudo || newPseudo }}
-            </p>
-            <p class="font-supercell text-white text-2xl">Hdv : {{ hdv }}</p>
-            <p class="font-supercell text-white text-2xl mb-6">
-                Ligue préférée : {{ league }}
-            </p>
-            <button
-                @click.prevent="newForm"
-                value="-"
-                class="w-full border border-beige hover:bg-beige transition duration-150 ease-in-out focus:ring-orange rounded-lg font-supercell text-beige hover:text-white px-8 py-4 text-lg focus:outline-none"
-            >
-              <p
-                  class="font-supercell text-white  text-2xl"
-              >
-                  Inscrire un autre compte
-              </p>
-            </button>
-        </div>
-    </div>
+                <h2 class="font-supercell text-white text-3xl mb-10">Récap</h2>
+                <p class="font-supercell text-white text-2xl">
+                    Pseudo : {{ pseudo || newPseudo }}
+                </p>
+                <p class="font-supercell text-white text-2xl">
+                    Hdv : {{ hdv }}
+                </p>
+                <p class="font-supercell text-white text-2xl mb-6">
+                    Ligue préférée : {{ league }}
+                </p>
+                <button
+                    @click.prevent="newForm"
+                    value="-"
+                    class="w-full border border-beige hover:bg-beige transition duration-150 ease-in-out focus:ring-orange rounded-lg font-supercell text-beige hover:text-white px-8 py-4 text-lg focus:outline-none"
+                >
+                    <p class="font-supercell text-white text-2xl">
+                        Inscrire un autre compte
+                    </p>
+                </button>
+            </div>
+        </section>
+    </form>
 </template>
 
 <script>
@@ -309,9 +302,7 @@ export default {
         toggleInput() {
             this.infoSubmit = false
         },
-        newForm() {
-          
-        }
+        newForm() {},
     },
 }
 </script>
